93f97ea86e8c6d8dacb97799f9f42dba44f9f494
rahulptl556 <rahulptl556@gmail.com>
2023-04-12 21:23:54 +0530

    css added with all components

--- Plan with love/src/App.js
+++ Plan with love/src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import data from './data'
+import Tours from "./components/Tours";
 
 const App = () => {
-  return <div>App</div>;
+
+  const [tours,setTours] = useState(data);
+
+  function removeTour(id){
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if(tours.length === 0){
+    <div>  
+      <h2>No tours Left</h2>
+      <button className="btn-refresh" onClick={() => setTours(data)}>
+        Refresh
+      </button>
+    </div>
+  }
+
+  return (  
+    // Wrapper
+
+    <div>
+      <Tours tours={tours} removeTour={removeTour}> </Tours>
+    </div>
+  )
 };
 
 export default App;
+ 
---
+++ Plan with love/src/components/Card.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function Card({id,image,info,price,name,removeTour}){
+
+    const[readmore,setReadmore] = useState(true);
+
+    const description = readmore ? `${info.substring(0,200)}....` : info;
+
+    function readmoreHandler(){
+        setReadmore(!readmore);
+    }
+
+    return(
+        <div className="card">
+
+            <img src={image} className="image"></img>
+
+            <div className="tour-info">
+                <div className="tour-details">
+                    <h4 className="tour-price"></h4>
+                    <h4 className="tour-name"></h4>
+                </div>
+
+                <div className="description">
+                    {description}
+                    <span className="read-more" onClick={readmoreHandler}>
+                        {readmore ? `Read More`: `Show Less`}
+                    </span>
+                </div>
+            </div>
+
+            <button className="btn-red" on Click = {() => removeTour(id)}>
+                Not Intrested
+            </button>
+
+
+
+        </div>
+    )
+}
+
+export default Card;
